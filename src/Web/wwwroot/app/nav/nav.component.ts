a4766731774c090b6adb2d94fb869fb95d73879e
Hem <HemTalreja@gmail.com>
2016-08-27 12:24:04 -0400

    browserify

---
+++ src/Web/wwwroot/app/nav/nav.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'ipg-nav',
+    template: `
+            <h4> IPG - Navigation Component View</h4>
+                <ng-content></ng-content>
+            `
+})
+export class NavComponent { }
