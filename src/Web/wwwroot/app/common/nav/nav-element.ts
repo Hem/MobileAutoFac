e18469cd0027ffb4171afa7ca5760cf9f3f2e1e2
Hem Talreja <HemTalreja@gmail.com>
2016-08-30 17:37:50 -0400

    reset app folder

--- src/Web/wwwroot/app/common/nav/nav-element.ts
+++
@@ -1,3 +0,0 @@
-﻿export class NavElement {
-    constructor(public id: any = 0, public name: string = '', public link: string = '') { }
-}
--- src/Web/wwwroot/app/main.ts
+++
@@ -1,9 +0,0 @@
-﻿import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { MainModule } from './main.module';
-
-
-// Bootstrap module here!!!
-// This way we can include the module on every page...
-platformBrowserDynamic().bootstrapModule( MainModule );
